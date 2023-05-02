0fc740dc355ecd4207e09f4a87b69d02b9f8b5a2
Kaleb Maravilla <=>
2023-05-02 15:06:01 -0600

    made the presidents file into a list

--- fact_check.py
+++ fact_check.py
@@ -1,2 +1,21 @@
 import csv
 
+def sort_presidents():
+    president_list = []
+    with open("presidents.txt", "r") as file:
+        reader = csv.reader(file, delimiter= ",")
+        for row in reader:
+            president_list.append(row)
+    print(president_list)
+            
+
+#with open("BLS_private.csv", "r", encoding="UTF8") as file:
+#    reader = csv.reader(file, delimiter=",")
+#    for row in reader:
+#        print(row)
+
+def main():
+    """This function will run the whole program"""
+    sort_presidents()
+#if __name__ = '__main__':
+main()
