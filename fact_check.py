e2675251b228b58d43c7eed4300838e05929e7a3
Kaleb Maravilla <=>
2023-05-02 16:25:32 -0600

    updated to the correct presidents text

--- fact_check.py
+++ fact_check.py
@@ -1,21 +1,24 @@
 import csv
 
-def sort_presidents():
-    president_list = []
+#def create_presidents_file(month, year):
+#    with open("presidents.txt", "w") as file:
+#        year_cnt = 1961
+#        for year in range(52):
+#            for month in range(1,13):
+#                file.writelines(f"democrat, {month}, {year_cnt}\n")
+#            year_cnt += 1
+
+def read_president_file(input_month, input_year):
     with open("presidents.txt", "r") as file:
         reader = csv.reader(file, delimiter= ",")
-        for row in reader:
-            president_list.append(row)
-    print(president_list)
-            
+        for party, month, year in reader:
+            if month == input_month and year == input_year:
+                print(party)
+                  
 
-#with open("BLS_private.csv", "r", encoding="UTF8") as file:
-#    reader = csv.reader(file, delimiter=",")
-#    for row in reader:
-#        print(row)
 
 def main():
     """This function will run the whole program"""
-    sort_presidents()
+    read_president_file("1", "1961")
 #if __name__ = '__main__':
 main()
